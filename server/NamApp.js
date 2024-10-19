84565a4fd24682965afec0d687065b74afe94e63
Yunho Nam <101496294+ManDuGuk@users.noreply.github.com>
2024-10-20 07:07:21 +0900

    20241020상품처리 관련 백단 작업중_테스트중

--- server/NamApp.js
+++ server/NamApp.js
@@ -4,11 +4,13 @@ const productRouter = require('./router/Product');
 const reviewRouter = require('./router/Review');
 const recentviewRouter = require('./router/recentviewRouter');
 const wishlistRouter = require('./router/wishlistRouter');
+const orderRouter = require('./router/orderRouter');
 
 const app = express();
 
 
 // 하위 요청이 상위로 올라가 있어야 /product에 안걸림
+app.use('/order', orderRouter); //http://localhost:3001/product/recentview
 app.use('/recentview', recentviewRouter); //http://localhost:3001/product/recentview
 app.use('/wishlist', wishlistRouter); //http://localhost:3001/product/wishlist
 app.use('/review', reviewRouter); //http://localhost:3001/product/review
